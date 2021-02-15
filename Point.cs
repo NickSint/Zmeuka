1674863705b129d4cfaf399fb19777bf1f47bd6b
Opilane TTHK <opilane@tthk.ee>
2021-02-15 14:29:15 +0200

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmeuka
+{
+    class Point //новый класс
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+
+
+        }
+
+        public void Draw()
+        {
+           Console.SetCursorPosition(x, y); //SetCursorPosition - начальная точка курсора
+           Console.Write(sym);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmeuka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1,3,'*'); //добавил класс
+            p1.Draw();
+
+            Point p2 = new Point(4,5 ,'#');
+            
+            p2.Draw();
+
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}    
